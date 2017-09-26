937ed891997bccdcc92ed9371ad6e95a3e5fe6a1
Dean Mc <VisuaGraph@kerneldba.com>
2017-09-26 23:19:43 +0100

    Workable part 2 solution

--- 2015/Day3.ps1
+++ 2015/Day3.ps1
@@ -5,13 +5,19 @@
 
 $PuzzleInput = get-content "$($env:USERPROFILE)\Documents\GIT\AdventOfCode\2015\Input\input3.txt"
 
+#RoboSanta
+$roboSanta = $true
+$whosTurn = 0 #defaults to santa
+
 #create hash table to mark co-ords
 $markCoOrds = @()
 
 $markCoOrds += ("0,0") #add first location
 
-$currentXCoOrd = 0 # simple x coord counter
-$currentYCoOrd = 0 # simple y coord counter
+$santaXCoOrd = 0 # simple x coord counter
+$santaYCoOrd = 0 # simple y coord counter
+$roboSantaXCoOrd = 0 # simple x coord counter
+$roboSantaYCoOrd = 0 # simple y coord counter
 
 $searchMaxCount = $puzzleInput.Length-1 #Find out the max number of times we search
 
@@ -19,20 +25,46 @@ for($i=0;$i -le $searchMaxCount;$i++) { #loop through every individual item; not
     $direction = $PuzzleInput[$i]
 
     #For each symbol we check what coord we need to update.
-    if($direction -eq "^"){
-        $currentYCoOrd++
+    if($whosTurn -eq 0){ #Santas turn
+        if($direction -eq "^"){
+            $santaYCoOrd++
+        }
+        elseif($direction -eq ">"){
+            $santaXCoOrd++
+        }
+        elseif($direction -eq "v"){
+            $santaYCoOrd--
+        }
+        elseif($direction -eq "<"){
+            $santaXCoOrd--
+        }
+        
+        #Update the array with the new direction taken.
+        $markCoOrds += "$santaXCoOrd,$santaYCoOrd"
     }
-    elseif($direction -eq ">"){
-        $currentXCoOrd++
+    
+    if($whosTurn -eq 1){
+        if($direction -eq "^"){
+            $roboSantaYCoOrd++
+        }
+        elseif($direction -eq ">"){
+            $roboSantaXCoOrd++
+        }
+        elseif($direction -eq "v"){
+            $roboSantaYCoOrd--
+        }
+        elseif($direction -eq "<"){
+            $roboSantaXCoOrd--
+        }
+        #Update the array with the new direction taken.
+        $markCoOrds += "$roboSantaXCoOrd,$roboSantaYCoOrd"
     }
-    elseif($direction -eq "v"){
-        $currentYCoOrd--
+    if($roboSanta -eq $true -and $whosTurn -eq 0){
+        $whosTurn=1 #switch to robo santa
     }
-    elseif($direction -eq "<"){
-        $currentXCoOrd--
+    else { #else default back to santa
+        $whosTurn=0
     }
-    #Update the array with the new direction taken.
-    $markCoOrds += "$currentXCoOrd,$currentYCoOrd"
 }
 
 $markCoOrds.Count #Count of directions taken.
